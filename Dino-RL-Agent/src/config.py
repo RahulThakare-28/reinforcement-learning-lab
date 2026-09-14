1e90c29877590244e48f3739cf5ef97806546fb6
RahulThakare-28 <rahulthakare6444@gmail.com>
2026-09-14 16:29:31 +0530

    configration variables

---
+++ Dino-RL-Agent/src/config.py
@@ -0,0 +1,15 @@
+# capture the game region 
+# Configuration
+
+GAME_REGION = {
+    "left": 950,
+    "top": 200,
+    "width": 950,
+    "height": 300
+}
+
+DINO_ROI_WIDTH = 180
+
+
+MODEL_PATH = "../models/dino_dqn_v1.zip"
+
