d878e34da9d0e31a63e87cbfe3f7652833df0f64
RahulThakare-28 <rahulthakare6444@gmail.com>
2026-09-14 16:29:01 +0530

    agent defination , train and save the agent

---
+++ Dino-RL-Agent/src/build_agent.py
@@ -0,0 +1,26 @@
+# train DQN on new environment version 2
+from stable_baselines3 import DQN
+from dino_env import DinoEnv
+
+env = DinoEnv()
+
+model = DQN(
+    policy="MlpPolicy",
+    env=env,
+    learning_rate=1e-3,
+    buffer_size=10_000,
+    learning_starts=1_000,
+    batch_size=32,
+    gamma=0.99,
+    exploration_fraction=0.2,
+    exploration_final_eps=0.05,
+    verbose=1
+)
+
+if __name__ == "__main__":
+
+    # train the model
+    model.learn(total_timesteps=100_000)
+
+    # save the model
+    model.save("../models/dino_dqn_v1")
